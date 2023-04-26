12e8c715aba3a3f9cff6364006fc5935f352db89
Gian Lucas <gianextensao@gmail.com>
2023-04-26 20:57:40 -0300

    feat: update files

---
+++ chamada/src/test/java/com/chamada/ChamadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
